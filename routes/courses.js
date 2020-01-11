57d5f08478082b13dd991fb529b8d8aee83f53ff
Raphael Ragoomundun <raphael.ragoomundun@protonmail.com>
2020-01-11 15:40:30 +0400

    update courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,13 +1,23 @@
 const router = require('express').Router({ mergeParams: true });
 
 // Bootcamps controller methods
-const { getCourses, getCourse, addCourse } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse
+} = require('../controllers/courses');
 
 router
   .route('/')
   .get(getCourses)
   .post(addCourse);
 
-router.route('/:id').get(getCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router;
