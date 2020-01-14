accaedb3912187fa998f8abe4d447cd289584b35
Raphael Ragoomundun <raphael.ragoomundun@protonmail.com>
2020-01-14 12:23:54 +0400

    controllers/auth.js: removed un-needed response return

--- controllers/auth.js
+++ controllers/auth.js
@@ -109,11 +109,6 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     return next(new ErrorResponse('Email could not be sent', 500));
   }
-
-  res.status(200).json({
-    success: true,
-    data: user
-  });
 });
 
 // Get token from model, create cookie and send response
