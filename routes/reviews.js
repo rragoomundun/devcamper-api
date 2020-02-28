2dd4f43ba3eaa0f45f8f14e98dbfc3cf9befefc3
Raphael Ragoomundun <raphael.ragoomundun@protonmail.com>
2020-02-28 17:02:56 +0400

    Review: addReview

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,20 +1,23 @@
 const router = require('express').Router({ mergeParams: true });
 
 // Courses controller methods
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 const advancedResults = require('../middlewares/advancedResults');
 
 const { protect, authorize } = require('../middlewares/auth');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description'
-  }),
-  getReviews
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description'
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id').get(getReview);
 
